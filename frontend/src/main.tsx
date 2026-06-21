298a269dafb85b04b24eb71ebc3a9b8b16d78104
root <root@LAPTOP-IKENSH3A.localdomain>
2026-06-21 19:17:10 +0800

    feat: port DA design tokens, themes, base, animations, markdown CSS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/base.css";
+import "./styles/animations.css";
+import "./styles/markdown.css";
 import App from "./App.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
